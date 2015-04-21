04137485b2f02996140eb2b6975fa69b275727bd
Ken Krueger <kenkrueger65@gmail.com>
2015-04-20 23:50:59 -0400

    latest

---
+++ lab-4-eureka-client-solution/src/main/java/demo/MyApplication.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
---
+++ lab-4-eureka-client-solution/src/main/java/demo/SentenceController.java
@@ -0,0 +1,42 @@
+package demo;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+
+
+@Controller
+public class SentenceController {
+
+	@Autowired DiscoveryClient client;
+	
+	@RequestMapping("/sentence")
+	public @ResponseBody String getSentence() {
+	  return 
+		getWord("LAB4SUBJECT") + " "
+		+ getWord("LAB4VERB") + " "
+		+ getWord("LAB4ARTICLE") + " "
+		+ getWord("LAB4ADJECTIVE") + " "
+		+ getWord("LAB4NOUN") + "."
+		;
+	}
+
+	public String getWord(String service) {
+        List<ServiceInstance> list = client.getInstances(service);
+        if (list != null && list.size() > 0 ) {
+      	URI uri = list.get(0).getUri();
+	      	if (uri !=null ) {
+	      		return (new RestTemplate()).getForObject(uri,String.class);
+	      	}
+        }
+        return null;
+	}
+}
