9e567890c10ef367d192f3a431a17c9ec199943d
Ken Krueger <kenkrueger65@gmail.com>
2015-05-20 21:57:28 -0400

    Lab 11

---
+++ lab-11/client/src/main/java/demo/LuckyWordController.java
@@ -0,0 +1,34 @@
+package demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ConfigurationProperties(prefix="wordConfig")
+public class LuckyWordController {
+	 
+	String luckyWord;
+	String preamble;
+	
+	@RequestMapping("/lucky-word")
+	public String showLuckyWord() {
+		return preamble + ": " + luckyWord;
+	}
+
+	public String getLuckyWord() {
+		return luckyWord;
+	}
+
+	public void setLuckyWord(String luckyWord) {
+		this.luckyWord = luckyWord;
+	}
+
+	public String getPreamble() {
+		return preamble;
+	}
+
+	public void setPreamble(String preamble) {
+		this.preamble = preamble;
+	}
+}
