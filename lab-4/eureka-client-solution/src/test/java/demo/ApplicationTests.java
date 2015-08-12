f6972b0fe03c92527a92d913d9f00b1afed7a581
Ken Krueger <kenkrueger65@gmail.com>
2015-08-11 21:03:19 -0400

    Added profile

--- lab-4/eureka-client-solution/src/test/java/demo/ApplicationTests.java
+++ lab-4/eureka-client-solution/src/test/java/demo/ApplicationTests.java
@@ -3,10 +3,12 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MyApplication.class)
+@ActiveProfiles("sentence")
 public class ApplicationTests {
 
 	@Test
