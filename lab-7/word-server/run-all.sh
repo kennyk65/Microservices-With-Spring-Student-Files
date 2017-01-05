2d2834570e38b6c7c58b85ba2eab155fee1a42cf
kennyk65 <kkrueger@gopivotal.com>
2017-01-05 15:47:11 -0500

    fixed maven command

--- lab-7/word-server/run-all.sh
+++ lab-7/word-server/run-all.sh
@@ -1,5 +1,5 @@
-mvn spring-boot:run -Dspring.profiles.active=subject &
-mvn spring-boot:run -Dspring.profiles.active=verb &
-mvn spring-boot:run -Dspring.profiles.active=article &
-mvn spring-boot:run -Dspring.profiles.active=adjective &
-mvn spring-boot:run -Dspring.profiles.active=noun 
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=subject" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=verb" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=article" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=adjective" &
+mvn spring-boot:run -Drun.jvmArguments="-Dspring.profiles.active=noun" 
