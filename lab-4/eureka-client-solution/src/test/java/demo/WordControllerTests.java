289da62214dc6634b12771b9c2d616e9973f8de1
Ken Krueger <kenkrueger65@gmail.com>
2018-05-14 10:14:27 -0400

    Simplified version of WordController, plus test

---
+++ lab-4/eureka-client-solution/src/test/java/demo/WordControllerTests.java
@@ -0,0 +1,32 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "words = foo,bar,baz")
+@AutoConfigureMockMvc
+public class WordControllerTests {
+
+	@Autowired
+	MockMvc mockMvc;
+
+
+	@Test
+	public void words() throws Exception {
+		mockMvc.perform(
+			get("/"))//
+			.andExpect(
+				content().string(anyOf(is("foo"), is("bar"), is("baz"))));
+	}
+
+}
