d200aa331ac9945354579204ea816157251059f6
Ken Krueger <kenkrueger65@gmail.com>
2018-07-16 14:35:47 -0400

    Establish test properties for local JUnit testing.

--- lab-8/client-solution/src/test/java/demo/ApplicationTests.java
+++ lab-8/client-solution/src/test/java/demo/ApplicationTests.java
@@ -3,11 +3,13 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@TestPropertySource(properties = { "wordConfig.luckyWord=foo", "wordConfig.preamble=bar" })
 @WebAppConfiguration
 public class ApplicationTests {
 
