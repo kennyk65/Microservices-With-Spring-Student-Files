4aabdaf71435eb49aa256e17f2911d2d2887b53e
Ken Krueger <kenkrueger65@gmail.com>
2018-07-16 14:20:58 -0400

    Added local-test profile to allow JUnit tests to run without a config server

--- lab-6/word-server/src/test/java/demo/ApplicationTests.java
+++ lab-6/word-server/src/test/java/demo/ApplicationTests.java
@@ -3,10 +3,13 @@ package demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
+@ActiveProfiles("local-test")
 public class ApplicationTests {
 
 	@Test
--- lab-9/word-server/src/test/java/demo/WordControllerTests.java
+++ lab-9/word-server/src/test/java/demo/WordControllerTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest(properties = "words = foo,bar,baz")
 @AutoConfigureMockMvc
+@ActiveProfiles("local-test")
 public class WordControllerTests {
 
 	@Autowired
