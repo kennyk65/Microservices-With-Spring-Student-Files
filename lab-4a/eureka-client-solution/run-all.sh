2bef50be045c555e6cc96d00d80c7d60bcc256cb
Ken Krueger <kenkrueger65@gmail.com>
2024-09-27 14:05:35 -0400

    remove lab 4 for now

--- lab-4a/eureka-client-solution/run-all.sh
+++
@@ -1,6 +0,0 @@
-mvn spring-boot:run -Dspring-boot.run.profiles=subject &
-mvn spring-boot:run -Dspring-boot.run.profiles=verb &
-mvn spring-boot:run -Dspring-boot.run.profiles=article &
-mvn spring-boot:run -Dspring-boot.run.profiles=adjective &
-mvn spring-boot:run -Dspring-boot.run.profiles=noun &
-mvn spring-boot:run -Dspring-boot.run.profiles=sentence
