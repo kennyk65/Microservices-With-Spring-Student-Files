8f1eb387a5ca57aa4bd3c38f56bb17271a903a6b
Ken Krueger <kenkrueger65@gmail.com>
2024-09-27 13:50:13 -0400

    staging

---
+++ lab-4a/eureka-client-solution/run-all.sh
@@ -0,0 +1,6 @@
+mvn spring-boot:run -Dspring-boot.run.profiles=subject &
+mvn spring-boot:run -Dspring-boot.run.profiles=verb &
+mvn spring-boot:run -Dspring-boot.run.profiles=article &
+mvn spring-boot:run -Dspring-boot.run.profiles=adjective &
+mvn spring-boot:run -Dspring-boot.run.profiles=noun &
+mvn spring-boot:run -Dspring-boot.run.profiles=sentence
---
+++ lab-4a/eureka-client-solution/src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,17 @@
+package demo;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("sentence")
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
