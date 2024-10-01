c25dcf1f54c53a92d3972d40662e3224e6e31386
Ken Krueger <kenkrueger65@gmail.com>
2024-10-01 12:38:32 -0400

    Switched Lab10 to HTTP Basic

---
+++ lab-10/word-server/src/main/java/demo/WordController.java
@@ -0,0 +1,20 @@
+package demo;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordController {
+
+    private final Random random = new Random();
+	
+	@Value("${words}") String[] words;
+	
+	@GetMapping("/")
+	public Word getWord() {
+		return new Word(words[random.nextInt(words.length)]);
+	}
+}
