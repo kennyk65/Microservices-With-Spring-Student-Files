2e5ddf9d249292c6ac860856e57392e2ecdabb2d
Ken Krueger <kenkrueger65@gmail.com>
2024-10-10 14:26:24 -0400

    imports

--- lab-11/sentence-server/src/main/java/demo/Application.java
+++ lab-11/sentence-server/src/main/java/demo/Application.java
@@ -3,8 +3,6 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
